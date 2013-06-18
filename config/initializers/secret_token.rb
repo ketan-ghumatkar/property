944e636bce064fb434b8e7d05d677f7ecc254990
Ketan Ghumatkar <ketan@weboniselab.com>
2013-06-19 00:06:59 +0545

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indianpropertywala::Application.config.secret_token = 'd93afa764989c724b8870c8aebb2f616934225bd96799b13532fd2339d61710d33ca776a8da784a0b0b9e55e306f6e6d779d39a7b253501394785eedfffaa7b4'
